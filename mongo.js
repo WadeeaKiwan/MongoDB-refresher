050e9ce8bde10b0b7b24442d1f7ba67ee905f9c4
WadeeaKiwan <wadeea.kiwan@hotmail.com>
2020-05-20 21:13:44 +0200

    Creating a Document with MongoDB

--- mongo.js
+++ mongo.js
@@ -2,7 +2,27 @@ const MongoClient = require("mongodb").MongoClient;
 
 const uri = process.env.MONGO_URI;
 
-const createProduct = async (req, res, next) => {};
+const createProduct = async (req, res, next) => {
+  const newProduct = {
+    name: req.body.name,
+    price: req.body.price
+  };
+
+  const client = new MongoClient(uri);
+
+  try {
+    await client.connect();
+
+    const db = client.db();
+
+    const result = db.collection("products").insertOne(newProduct);
+  } catch (error) {
+    return res.json({ message: "Could not store data" });
+  }
+  client.close();
+
+  res.json(newProduct);
+};
 
 const getProducts = async (req, res, next) => {};
 
