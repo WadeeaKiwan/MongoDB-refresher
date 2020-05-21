a8487de459b004416135cf4c7312f6ea327574ef
WadeeaKiwan <wadeea.kiwan@hotmail.com>
2020-05-21 11:46:23 +0200

    Getting Products

--- mongoose.js
+++ mongoose.js
@@ -3,7 +3,10 @@ const mongoose = require("mongoose");
 const Product = require("./models/product");
 
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
   .then(() => console.log("Connected to database!"))
   .catch(() => console.log("Connection failed!"));
 
@@ -18,4 +21,11 @@ const createProduct = async (req, res, next) => {
   res.json(result);
 };
 
+const getProducts = async (req, res, next) => {
+  const products = await Product.find().exec(); // .exec() turns find() to a real promise
+
+  res.json(products);
+};
+
 exports.createProduct = createProduct;
+exports.getProducts = getProducts;
